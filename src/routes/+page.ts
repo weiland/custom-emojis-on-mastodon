9abacc70253e15385a386945f48cf8a025ad3487
Pascal Weiland <weiland@users.noreply.github.com>
2023-08-09 10:26:02 +0200

    Add more styling

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,17 +1,49 @@
 import type { PageLoad } from './$types';
 
-export const load = (async ({ fetch, url, params }) => {
+export type Category = string;
+
+export type Emoji = {
+	shortcode: string,
+	url: string,
+	static_url: string,
+	visible_in_picker: boolean,
+	category: Category
+};
+
+export const load = (async ({ fetch, url }) => {
 
 	const hostname = url.searchParams.get('hostname');
-	console.log('hostname', hostname)
 	if (!hostname) {
-		return { error: 'no hostname provided' };
+		return {};
+		// return { error: 'Please provide a hostname.' };
 	}
-	const res = await fetch(`https://${hostname}/api/v1/custom_emojis`);
-	const json = await res.json();
 
-	// const emojis = json.map(({ shortcode, url, category }) => ({ shortcode, url, category }));
-	const emojis = json.reduce((acc, item) => {
+	if (!/\./.test(hostname) || hostname.length <= 3) {
+		return { error: 'Please provide a valid hostname.' };
+	}
+
+	const emojisUrl = `https://${hostname}/api/v1/custom_emojis`;
+	let res;
+	try {
+		res = await fetch(emojisUrl);
+	} catch (error) {
+		console.error('Error during fetch:', error);
+		return { error: `Could not fetch Emoji list from hostname "${hostname}".` };
+	}
+
+	let json: Emoji[];
+	try {
+		json = await res.json();
+	} catch (error) {
+		console.error('Error during json parsing:', error);
+		return { error: `Could not parse emoji list from "${emojisUrl}".` };
+	}
+
+	if (!Array.isArray(json) || json.length === 0 || typeof json[0].shortcode === 'undefined') {
+		return { error: 'Could not find any emojis. :c' };
+	}
+
+	const emojis: Record<Category, Emoji[]> = json.reduce((acc: Record<Category, Emoji[]>, item: Emoji) => {
 		const { category } = item;
 		if (acc[category]) {
 			acc[category].push(item);
@@ -22,13 +54,5 @@ export const load = (async ({ fetch, url, params }) => {
 	}, {});
 
 	return { emojis, hostname };
-
-	// try {
-	//         const res = await fetch(`/emojis`);
-	//         const item = await res.json();
-	//         return { item };
-	// } catch (error) {
-	//         return { error };
-	// }
 
 }) satisfies PageLoad;
